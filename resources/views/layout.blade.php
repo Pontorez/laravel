7092637517a90ec65d649c842faff14ffe252d4f
Pontorez <pontorez@pontorez.com>
2019-01-09 13:40:29 -0500

    Add some space below "Go to main page" button

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,6 +5,11 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="/css/bootstrap.min.css" rel="stylesheet"/>
+    <style>
+        footer {
+            padding-bottom: 20px;
+        }
+    </style>
 </head>
 <body>
 
